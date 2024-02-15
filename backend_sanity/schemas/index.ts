0b286c6fce5bb3a6987de6894a5a377ddc18908e
Jacob <jacobbailly2002@gmail.com>
2024-02-14 23:38:28 -0500

    Initial commit

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,11 @@
+export const schemaTypes = [testimonials, abouts, works, brands, experiences, skills, workExperience, contact]
+
+//import createSchema from 'part:@sanity/base/schema-creator'
+import testimonials from './testimonials'
+import abouts from './abouts'
+import works from './works'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
